6b745fb92013cd38b79d090ca15203ff6b4c64ec
Ngandos <76482435+Ngandos@users.noreply.github.com>
2023-07-11 11:16:39 +0200

    Code Clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import { useState } from "react"
-import Cart from "./composants/Cart"
-import ArticleList from "./composants/ArticleList"
-import Header from "./composants/Header"
-import Footer from "./composants/Footer"
-import Articles from "./composants/Article"
-import SearchBar from "./composants/SearchBar"
-import CategorieList from "./composants/CategorieList";
-import './App.css'
-import MenuNav from "./composants/MenuNav/MenuNav"
+import { useState } from "react";
+import Cart from "./composants/Cart";
+import ArticleList from "./composants/ArticleList";
+import Header from "./composants/Header";
+import Footer from "./composants/Footer";
+import SearchBar from "./composants/SearchBar";
+import './App.css';
+import MenuNav from "./composants/MenuNav/MenuNav";
 
 function App() {
 
@@ -26,7 +24,6 @@ function App() {
       <Footer viewCart={false} />
     </div>
   )
-  console.log(Articles, CategorieList)
 
   return  content
 }
